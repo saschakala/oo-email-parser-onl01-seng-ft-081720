e1450e63531900958400414a578ad50b692351b9
“saschakala” <mxsaschakala@gmail.com>
2020-08-29 17:36:38 -0600

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,17 @@
 class EmailAddressParser
+attr_accessor :emails
 
-    attr_accessor :email
-    def  initialize(emails)
-      @email = emails
-    end
-  
-    def parse
-      email_array = @email.split(/[, ]/).uniq
-      email_array.reject! {|element| element.empty?}
-      email_array
-    end
-  
-  end
+def initialize (emails)
+  @emails = emails
+end
+
+def parse
+  email_array = @emails.split(/[, ]/).uniq
+  email_array.reject! {|element| element.empty?}
+  email_array
+end
+
+
+end
+
+
